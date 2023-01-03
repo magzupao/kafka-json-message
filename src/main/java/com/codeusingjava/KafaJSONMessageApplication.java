f4e174ecd6aef976415f9623875c686afa1c8410
magzupao <mguado@gmail.com>
2023-01-03 22:42:09 +0100

    logger en app

--- src/main/java/com/codeusingjava/KafaJSONMessageApplication.java
+++ src/main/java/com/codeusingjava/KafaJSONMessageApplication.java
@@ -1,5 +1,8 @@
 package com.codeusingjava;
 
+import com.codeusingjava.controller.ProducerController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,13 +13,14 @@ import com.codeusingjava.util.AppConstants;
 @SpringBootApplication
 public class KafaJSONMessageApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(KafaJSONMessageApplication.class);
 	public static void main(String[] args) {
         SpringApplication.run(KafaJSONMessageApplication.class, args);
     }
 
 	@KafkaListener(topics = AppConstants.TOPIC, groupId=AppConstants.GROUP_ID)
     public void listen(Person user) {
-        System.out.println("Received info--> : " +user);
-        System.out.println("Received info ************************** --> : " +user.getName() );
+        logger.info("Received info--> : ", user);
+        logger.info("Received info ************************** --> : ", user.getName() );
     }
 }
